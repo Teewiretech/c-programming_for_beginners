e6a337676f6e82016c9f79250786f9f0c967805f
Teewiretech <taiwomutiu1998@yahoo.com>
2023-09-14 05:19:08 -0700

    myfirstproject

---
+++ myfirstproject/0-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success).
+ *
+ */
+
+/* I will be prosper in coding */
+
+int main(void)
+{
+	unsigned int a;
+
+	a = -32769;
+	
+		printf("%d", a);
+	
+	return (0);
+}
